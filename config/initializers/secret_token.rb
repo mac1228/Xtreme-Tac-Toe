199fe5c0ad60525109b0d7d8924bb65aa41fb687
Michael Curd <mac1228@gmail.com>
2012-09-19 11:51:56 -0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+XtremeTacToe::Application.config.secret_token = '3ff7f69399609ea09cfb331a06a6049841c290d9bcaa39bb43cc040efdfb93e5223acf64eb9927e13cd55cfb2ee5c16d483ac602e82f9c980b5aab783ebd64d1'
